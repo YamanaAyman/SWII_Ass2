23e140cce611ddb54e9ea6d25cc0aba4b1236fe8
Nooranshibl <34580774+Nooranshibl@users.noreply.github.com>
2019-03-09 18:39:22 +0200

    Add files via upload

--- MyBlockChain.java
+++ MyBlockChain.java
@@ -1,61 +1,54 @@
-
-package myblockchain;
-
 import java.util.ArrayList;
 import java.util.Arrays ;
 import java.text.SimpleDateFormat ;
 
 public class MyBlockChain {
-   
-    public static ArrayList<Block > Chain= new ArrayList() ;  
 
-public class MyChain
- {      
- 
-     public void Create_genesisBlock()
-    {      
-        String  transaction="yamana sends 100 bitcoin " ;
-              Block b ;
-         b = new Block(0,"1/1/2018","0",transaction );
-       
-               Chain.add(b);
-    }
-    MyChain()
+    public static ArrayList<Block > Chain= new ArrayList() ;
+
+    public class MyChain
     {
-      Create_genesisBlock();
-    }
-    
-      public void Add_Block(Block bb)
-      {
-          String myHash=bb.Calculate_Hash(bb.Get_trans()) ;
-          Chain.add(bb);
-      }
+        public void Create_genesisBlock()
+        {
+            String  transaction="yamana sends 100 bitcoin " ;
+            Block b ;
+            b = new Block(0,"1/1/2018","0",transaction );
 
-     public boolean IsValid()
-     {
-        for (int i=1; i<Chain.size() ;i++)
-        { 
-           Block currentBlock=Chain.get(i);
-           Block PreviousBlock=Chain.get(i-1);
-           if(PreviousBlock.Get_BlockHash()!=currentBlock.GetPrev_hash())
-               return false ;
-           if(currentBlock.Get_BlockHash()!= currentBlock.Calculate_Hash(currentBlock.Get_trans() )) {
-            } else {
-               return false ;
-            }
+            Chain.add(b);
         }
-       return true ;
-     }
+        MyChain()
+        {
+            Create_genesisBlock();
+        }
 
-}
- 
+        public void Add_Block(Block bb)
+        {
+            bb.Mining(5);
+            Chain.add(bb);
+        }
 
-    
+        public boolean IsValid()
+        {
+            for (int i=1; i<Chain.size() ;i++)
+            {
+                Block currentBlock=Chain.get(i);
+                Block PreviousBlock=Chain.get(i-1);
+                if(PreviousBlock.Get_BlockHash()!=currentBlock.GetPrev_hash())
+                    return false ;
+                if(currentBlock.Get_BlockHash()!= currentBlock.Calculate_Hash(currentBlock.Get_trans() )) {
+                } else {
+                    return false ;
+                }
+            }
+            return true ;
+        }
+    }
+
     public static void main(String[] args) {
-    
+
 //   String[] transactions={"yamana sends 100 bitcoin ","Murat sends 20 bitcoin to hayat"} ;
 //    Block genesis_Block=new Block( 0,"3/3/2019",0 , transactions) ;
-//     
+//
 //    String[] transactions2={"Can sends 10 bitcoin ", "kemal sends 200 bitcoin to ferah"} ;
 //    Block Block2=new Block(1,"10/3/2019",genesis_Block.Get_BlockHash()  , transactions2) ;
 //    System.out.println("hash of genesis block ");
@@ -64,5 +57,5 @@ public class MyChain
 //    System.out.println(Block2.Get_BlockHash()) ;
 
     }
-    
+
 }
