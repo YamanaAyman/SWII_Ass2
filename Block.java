a409e1730540e7eae8fec4ccf6b4032dc744dc6b
YamanaAyman <45406867+YamanaAyman@users.noreply.github.com>
2019-03-09 18:23:08 +0200

    Add files via upload

---
+++ Block.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myblockchain;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.security.MessageDigest;
+import javax.xml.bind.DatatypeConverter ;
+
+/**
+ *
+ * @author wi7
+ */
+public class Block {
+    
+        public int index ;    // number of block 
+        public String timeStamp ;   // emty el block created 
+        private String prev_hash ;
+        private  String trans ; // data is an Amount
+        private String Block_hash ;
+
+     
+    Block(int index, String timeStamp ,String prev_hash,String trans)
+       { 
+         this.index=index; 
+         this.timeStamp= timeStamp ;
+         this.prev_hash=prev_hash ;
+         this.trans=trans ;
+        
+          
+       }   
+    public  String Calculate_Hash(byte[] bb)
+    {
+//        Object[] content ={Arrays.hashCode(trans), prev_hash };
+//       return(   this.Block_hash=Arrays.hashCode(content) ) ; 
+        String hash_value="";
+        String str2="SHA-256" ;
+        try
+         {
+           MessageDigest message=MessageDigest.getInstance(str2);
+           message.update(bb);
+           byte[] digestedBytes=message.digest();
+           hash_value=DatatypeConverter.printHexBinary(digestedBytes).toLowerCase();
+        
+        
+        
+          }
+        catch(Exception e) {}
+        return hash_value ;  
+    }
+    
+    public String GetPrev_hash()
+       { 
+           
+        return prev_hash ;
+       }
+    public byte[] Get_trans()
+    {      
+        String s=trans ;   
+       return (s.getBytes());
+    }
+    public String Get_BlockHash()
+    {
+       return Block_hash ;
+    }
+
+
+//      public int Get_Hash(int x) {
+//      
+//      }
+
+}
